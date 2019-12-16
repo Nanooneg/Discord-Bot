3807b85b4c156458cc59b2f52e4ab764e7afc911
nanoo <arnaudlaval33@gmail.com>
2019-12-16 15:06:11 +0100

    first release

--- src/main/java/com/nanoo/discord/AppMain.java
+++ src/main/java/com/nanoo/discord/AppMain.java
@@ -3,14 +3,16 @@ package com.nanoo.discord;
 import com.jagrosh.jdautilities.command.CommandClient;
 import com.jagrosh.jdautilities.command.CommandClientBuilder;
 import com.jagrosh.jdautilities.commons.waiter.EventWaiter;
-import com.nanoo.discord.commands.*;
+import com.nanoo.discord.commands.CoffeeFilterToggle;
+import com.nanoo.discord.commands.Image;
+import com.nanoo.discord.commands.ServerInfo;
+import com.nanoo.discord.commands.UserInfo;
 import com.nanoo.discord.config.Bot;
 import com.nanoo.discord.events.CategoryCreate;
 import com.nanoo.discord.events.HelloEvent;
 import com.nanoo.discord.filters.CoffeeFilter;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
-import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
 
 import javax.security.auth.login.LoginException;
